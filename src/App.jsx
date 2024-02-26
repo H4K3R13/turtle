404c55b875d73748b7675dda12e191851a205919
H4K3R13 <arjunmundamani@gmail.com>
2024-02-26 20:25:38 +0530

    Fixed the layout size, Added  Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import { useState } from "react";
 import turtleLogo from "../public/turtle.png";
 import Add from "./components/Add";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  console.log("Hello Testing");
   return (
     <>
-      <div className="header">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={turtleLogo} className="logo" alt="turtle logo" />
-        </a>
-        <h1>turtle</h1>
-      </div>
+      <Navbar />
       <Add />
     </>
   );
