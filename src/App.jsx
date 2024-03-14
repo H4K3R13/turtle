4b413e56c07a5ab8a790809657e69583b7777b8c
H4K3R13 <arjunmundamani@gmail.com>
2024-03-14 23:20:21 +0530

    Commented Out Focus

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,24 @@ import { useEffect } from 'react';
 
 function App() {
 
-  useEffect(() => {
-    const handleFocus = () => {
-      console.log('Focused');
-    };
+  // This useEffect() is to keep the extension tab in focus (unclosed) when clicked outside the extension box. 
+  // useEffect(() => {
+  //   const handleFocus = () => {
+  //     console.log('Focused');
+  //   };
 
-    const handleBlur = () => {
-      console.log('Blurred');
-    };
+  //   const handleBlur = () => {
+  //     console.log('Blurred');
+  //   };
 
-    window.addEventListener('focus', handleFocus);
-    window.addEventListener('blur', handleBlur);
+  //   window.addEventListener('focus', handleFocus);
+  //   window.addEventListener('blur', handleBlur);
 
-    return () => {
-      window.removeEventListener('focus', handleFocus);
-      window.removeEventListener('blur', handleBlur);
-    };
-  }, [])
+  //   return () => {
+  //     window.removeEventListener('focus', handleFocus);
+  //     window.removeEventListener('blur', handleBlur);
+  //   };
+  // }, [])
 
 
   return (
